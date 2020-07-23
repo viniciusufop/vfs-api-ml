50d25b6fd25dee46cde4bacd8d5536952787b507
Vinicius Faria de Souza <vinicius.faria@cvortex.io>
2020-07-23 17:26:35 -0300

    criando handle basico de tratativa de errors

---
+++ src/main/java/br/com/vfs/api/ml/shared/errors/ErrorHandler.java
@@ -0,0 +1,41 @@
+package br.com.vfs.api.ml.shared.errors;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@RestControllerAdvice
+public class ErrorHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(final MethodArgumentNotValidException ex,
+                                            final HttpHeaders headers, HttpStatus status, final WebRequest request) {
+        var errors = getErrors(ex);
+        log.info("M=handleMethodArgumentNotValid, errors:{}", errors);
+        var url = ((ServletWebRequest) request).getRequest().getRequestURI();
+        var errorMessage = new ErrorMessage(errors, url);
+        return super.handleExceptionInternal(ex, errorMessage, headers, status, request);
+    }
+
+    private List<String> getErrors(final MethodArgumentNotValidException ex) {
+        return ex.getBindingResult().getFieldErrors()
+                .stream()
+                .map(this::getFinalMessage)
+                .collect(Collectors.toList());
+    }
+
+    private String getFinalMessage(final FieldError fieldError) {
+        return String.format("field: %s, message: %s",fieldError.getField(), fieldError.getDefaultMessage());
+    }
+}
