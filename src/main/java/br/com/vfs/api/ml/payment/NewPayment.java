823decbf6925e04b44ce6438e55cf6225b1017e6
Vinicius Faria de Souza <vinicius.faria@cvortex.io>
2020-08-12 19:32:42 -0300

    removendo import descenessario

--- src/main/java/br/com/vfs/api/ml/payment/NewPayment.java
+++ src/main/java/br/com/vfs/api/ml/payment/NewPayment.java
@@ -1,13 +1,6 @@
 package br.com.vfs.api.ml.payment;
 
 import br.com.vfs.api.ml.purchase.Purchase;
-import br.com.vfs.api.ml.purchase.PurchaseRepository;
-import br.com.vfs.api.ml.shared.annotations.ExistElement;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 public interface NewPayment {
     Long getIdPurchase();
